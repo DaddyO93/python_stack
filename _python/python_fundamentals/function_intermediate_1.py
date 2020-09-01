5c375f04af0c9a398070944cb35f9bed97fc4d8f
Russ Lance <russl.wbcc@gmail.com>
2020-09-01 18:43:41 -0500

    add bonus challenge code

--- _python/python_fundamentals/function_intermediate_1.py
+++ _python/python_fundamentals/function_intermediate_1.py
@@ -1,30 +1,17 @@
 import random
 def randInt(min=0, max=100):
-    num = random.random()*max
-    return round(num)
+    if min>max:
+        print("Min must be higher than Max!")
+    elif max<0:
+        print("Max must be greater than 0")
+    else:
+        num = random.random()*(max-min)+min
+        return round(num)
 
 print(randInt()) 		# should print a random integer between 0 to 100
 
-
-import random
-def randInt(min=0, max=100):
-    num = random.random()*max
-    return round(num)
-
 print(randInt(max=50)) 	# should print a random integer between 0 to 50
 
-
-import random
-def randInt(min=0, max=100):
-    num = random.random()*(max-min)+min
-    return round(num)
-
 print(randInt(min=50)) 	# should print a random integer between 50 to 100
-
-
-import random
-def randInt(min=0, max=100):
-    num = random.random()*(max-min)+min
-    return round(num)
 
 print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
