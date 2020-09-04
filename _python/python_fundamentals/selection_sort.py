5d2d280b7105bd88906ab7a67f094d462c73fd70
Russ Lance <russl.wbcc@gmail.com>
2020-09-03 19:15:48 -0500

    add selection sort

--- _python/python_fundamentals/selection_sort.py
+++ _python/python_fundamentals/selection_sort.py
@@ -1,10 +1,11 @@
 x = [1,2,4,6,8,2,34,6,9,345,8,245,78657,1,32,134,75]
 
 def selection_sort(x):
-    lowest = x[0]
     for start in range(len(x)-1):
-        for i in range(len(x)-1-start):
-            if x[i]<lowest:
-                x[start]=x[i]
+        lowest = start
+        for i in range(start, len(x)):
+            if x[i]<x[lowest]:
+                lowest=i
+        x[lowest], x[start]=x[start], x[lowest]
     return x
 print(selection_sort(x))
