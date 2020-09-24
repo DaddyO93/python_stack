a96387cb41559b0bb1f1a4485b8168309e378ef9
DaddyO93 <russl.wbcc@gmail.com>
2020-09-23 20:35:22 -0500

    add user app

---
+++ django/django_orm/single_model_orm/users_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('process_info', views.process_info)
+]
---
+++ django/django_orm/single_model_orm/users_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect
+from .models import User
+
+# Create your views here.
+def index (request):
+    context = {
+        "all_users":User.objects.all()
+    }
+    return render (request, 'index.html', context)
+
+def process_info (request):
+    print(request.POST)
+    User.objects.create(first_name=request.POST['first_name'], last_name=request.POST['last_name'], email_address=request.POST['email_address'], age=request.POST['age'])
+    return redirect('/')
